ce6c2bd64e4d916639ff4eaacf010b42b90169bb
chldmswnl <chldmswnl12@gmail.com>
2021-08-08 07:56:35 +0900

    edit readme

--- types/index.d.ts
+++ types/index.d.ts
@@ -9,15 +9,4 @@ declare module "Mymodule" {
     language: string;
     borderCountries: string[];
   };
-
-  type rType = {
-    img: string;
-    name: string;
-    population: string;
-    capital: string;
-    region: string;
-    currency: string;
-    language: string;
-    borderCountries: string[];
-  };
 }
